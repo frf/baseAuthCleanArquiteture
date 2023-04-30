e6923535f2b98e58320fe1ff0a4e55cf75d5773f
Fabio Rocha <fabio.farias@tegra.com.br>
2023-04-30 19:04:41 -0300

    refactor: teste mock

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,12 +1,34 @@
 /// Imports
-import { describe, it, expect } from 'vitest';
+import { describe, it, expect, vitest } from 'vitest';
 import { SignUpController } from './signup';
 import { MissingParamError } from '../errros/missiing-param-error';
+import { InvalidParamError } from '../errros/invalid-param-error';
+import { EmailValidator } from '../protocols/email-validator';
 
+interface SutTypes {
+  sut: SignUpController;
+  emailValidatorStub: EmailValidator;
+}
+
+const makeSut = (): SutTypes => {
+  class EmailValidatorStub implements EmailValidator {
+    isValid(email: string): boolean {
+      return false;
+    }
+  }
+
+  const emailValidatorStub = new EmailValidatorStub();
+  const sut = new SignUpController(emailValidatorStub);
+
+  return {
+    sut,
+    emailValidatorStub
+  };
+};
 // Tests
 describe('Signup Controller', async () => {
   it('Shoud return 400 if no name is provided', async () => {
-    const sut = new SignUpController();
+    const { sut } = makeSut();
     const httpRequest = {
       body: {
         email: 'any_email@email.com',
@@ -19,7 +41,7 @@ describe('Signup Controller', async () => {
     expect(httpResponse.body).toEqual(new MissingParamError('name'));
   });
   it('Shoud return 400 if no email is provided', async () => {
-    const sut = new SignUpController();
+    const { sut } = makeSut();
     const httpRequest = {
       body: {
         name: 'any_name',
@@ -32,7 +54,7 @@ describe('Signup Controller', async () => {
     expect(httpResponse.body).toEqual(new MissingParamError('email'));
   });
   it('Shoud return 400 if no password is provided', async () => {
-    const sut = new SignUpController();
+    const { sut } = makeSut();
     const httpRequest = {
       body: {
         name: 'any_name',
@@ -45,7 +67,7 @@ describe('Signup Controller', async () => {
     expect(httpResponse.body).toEqual(new MissingParamError('password'));
   });
   it('Shoud return 400 if no password confirmation is provided', async () => {
-    const sut = new SignUpController();
+    const { sut } = makeSut();
     const httpRequest = {
       body: {
         name: 'any_name',
@@ -58,5 +80,22 @@ describe('Signup Controller', async () => {
     expect(httpResponse.body).toEqual(
       new MissingParamError('passwordConfirmation')
     );
+  });
+
+  it('Shoud return 400 if email invalid is provided', async () => {
+    const { emailValidatorStub, sut } = makeSut();
+    vitest.spyOn(emailValidatorStub, 'isValid').mockReturnValueOnce(false);
+
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'invalid_email@email.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    };
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new InvalidParamError('email'));
   });
 });
--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,9 +1,17 @@
+import { InvalidParamError } from '../errros/invalid-param-error';
 import { MissingParamError } from '../errros/missiing-param-error';
 import { badRequest, successResponse } from '../helpers/http-helpers';
 import { Controller } from '../protocols/controller';
+import { EmailValidator } from '../protocols/email-validator';
 import { HttpRequest, HttpResponse } from '../protocols/http';
 
 export class SignUpController implements Controller {
+  private readonly emailValidator: EmailValidator;
+
+  constructor(emailValidator: EmailValidator) {
+    this.emailValidator = emailValidator;
+  }
+
   handle(httpRequest: HttpRequest): HttpResponse {
     const requiredFields = [
       'name',
@@ -16,6 +24,12 @@ export class SignUpController implements Controller {
       if (!httpRequest.body[field]) {
         return badRequest(new MissingParamError(field));
       }
+    }
+
+    const isValidEmail = this.emailValidator.isValid(httpRequest.body.email);
+
+    if (!isValidEmail) {
+      return badRequest(new InvalidParamError('email'));
     }
 
     return successResponse({});
