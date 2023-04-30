9a049d1e5f88543834ff59802623c3c1354b2c01
Fabio Rocha <fabio.farias@tegra.com.br>
2023-04-30 18:09:42 -0300

    chore: config

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,20 +1,20 @@
 /// Imports
-import { describe, it, expect } from "vitest";
-import { SignUpController } from "./signup";
+import { describe, it, expect } from 'vitest';
+import { SignUpController } from './signup';
 
 // Tests
-describe("Signup Controller", async () => {
-  it("Shoud return 400 if no name id provided", async () => {
+describe('Signup Controller', async () => {
+  it('Shoud return 400 if no name id provided', async () => {
     const sut = new SignUpController();
     const httpRequest = {
       body: {
-        name: "any_name",
-        email: "any_email@email.com",
-        password: "any_password",
-        passwordConfirmation: "any_password",
-      },
+        email: 'any_email@email.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
     };
     const httpResponse = sut.handle(httpRequest);
     expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new Error('Missing param: name'));
   });
 });
